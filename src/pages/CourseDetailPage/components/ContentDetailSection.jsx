188b21640f162ba3147b4bb5940b89df9d4ee9ba
Tommy Nguyen <nguyenanhnhat123456@gmail.com>
2024-07-29 19:39:28 +0700

    Fix course detail page font-size

--- src/pages/CourseDetailPage/components/ContentDetailSection.jsx
+++ src/pages/CourseDetailPage/components/ContentDetailSection.jsx
@@ -16,17 +16,19 @@ const ContentDetailSection = ({
       content: item?.description || "",
     };
   });
+
   return (
-    <section className="contentdetail">
-      <div className="content">
-        <div className="container">
-          <div className="contentrow ctintro">
-            <h3 className="contentrow__title title --t3">Giới thiệu</h3>
-            <div
-              className="contenteditor"
-              dangerouslySetInnerHTML={{ __html: description }}
-            >
-              {/* <h2
+    <>
+      <section className="contentdetail">
+        <div className="content">
+          <div className="container">
+            <div className="contentrow ctintro">
+              <h3 className="contentrow__title title --t3">Giới thiệu</h3>
+              <div
+                className="contenteditor"
+                dangerouslySetInnerHTML={{ __html: description }}
+              >
+                {/* <h2
                 style={{
                   fontSize: "inherit",
                   margin: "inherit",
@@ -100,35 +102,37 @@ const ContentDetailSection = ({
                   allowFullScreen="allowfullscreen"
                 />
               </div> */}
+              </div>
             </div>
-          </div>
-          <div className="contentrow ctschedule">
-            <h3 className="contentrow__title title --t3">Lịch học</h3>
-            <div className="ctschedule__box">
-              <div className="info">
-                <div className="labeltext">
-                  <span className="label --blue">Khai giảng</span>
-                  <p className="title --t3">{startDate || ""}</p>
-                </div>
-                <div className="labeltext">
-                  <span className="label --blue">Ngày học</span>
-                  <p className="title --t3">{schedule?.days || ""}</p>
-                </div>
-                <div className="labeltext">
-                  <span className="label --blue">Thời gian</span>
-                  <p className="title --t3">{schedule?.time || ""}</p>
-                </div>
-                <div className="labeltext">
-                  <span className="label --blue">Địa điểm</span>
-                  <p className="title --t3">{schedule?.address || ""}</p>
+            <div className="contentrow ctschedule">
+              <h3 className="contentrow__title title --t3">Lịch học</h3>
+              <div className="ctschedule__box">
+                <div className="info">
+                  <div className="labeltext">
+                    <span className="label --blue">Khai giảng</span>
+                    <p className="title --t3">{startDate || ""}</p>
+                  </div>
+                  <div className="labeltext">
+                    <span className="label --blue">Ngày học</span>
+                    <p className="title --t3">{schedule?.days || ""}</p>
+                  </div>
+                  <div className="labeltext">
+                    <span className="label --blue">Thời gian</span>
+                    <p className="title --t3">{schedule?.time || ""}</p>
+                  </div>
+                  <div className="labeltext">
+                    <span className="label --blue">Địa điểm</span>
+                    <p className="title --t3">{schedule?.address || ""}</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="contentrow ctlession">
-            <h3 className="contentrow__title title --t3">Nội dung khoá học</h3>
-            {!!content?.length && <Accordion data={modifiedContent} />}
-            {/* <div className="accordion">
+            <div className="contentrow ctlession">
+              <h3 className="contentrow__title title --t3">
+                Nội dung khoá học
+              </h3>
+              {!!content?.length && <Accordion data={modifiedContent} />}
+              {/* <div className="accordion">
               <div className="accordion__content">
                 <div className="accordion__content-title">
                   <h4>
@@ -1877,44 +1881,44 @@ const ContentDetailSection = ({
                 </div>
               </div>
             </div> */}
-          </div>
-          <div className="contentrow ctrequest">
-            <h3 className="contentrow__title title --t3">Yêu cầu cần có</h3>
-            <div className="ctrequest__content">
-              {!!required?.length &&
-                required.map((item, index) => {
-                  return <p key={index}>{item}</p>;
-                })}
             </div>
-          </div>
-          <div className="contentrow ctteacher">
-            <h3 className="contentrow__title title --t3">Giảng viên</h3>
-            <div className="ctteacher__content">
-              {!!teams?.length &&
-                teams.map((team, index) => {
-                  return (
-                    <div key={team?.id || index} className="itemteacher">
-                      <div className="itemteacher__avatar">
-                        <img src={team?.image} alt="CFD Circle" />
-                      </div>
-                      <div className="itemteacher__info">
-                        <div className="itemteacher__info-name">
-                          <p className="title --t3">{team?.name}</p>
-                          <span className="label badge --teacher">
-                            {team?.tags[0]}
-                          </span>
+            <div className="contentrow ctrequest">
+              <h3 className="contentrow__title title --t3">Yêu cầu cần có</h3>
+              <div className="ctrequest__content">
+                {!!required?.length &&
+                  required.map((item, index) => {
+                    return <p key={index}>{item}</p>;
+                  })}
+              </div>
+            </div>
+            <div className="contentrow ctteacher">
+              <h3 className="contentrow__title title --t3">Giảng viên</h3>
+              <div className="ctteacher__content">
+                {!!teams?.length &&
+                  teams.map((team, index) => {
+                    return (
+                      <div key={team?.id || index} className="itemteacher">
+                        <div className="itemteacher__avatar">
+                          <img src={team?.image} alt="CFD Circle" />
                         </div>
-                        <h5 className="itemteacher__info-pos label">
-                          {team?.jobTitle}
-                        </h5>
-                        <p className="itemteacher__info-des">
-                          {team?.description}
-                        </p>
+                        <div className="itemteacher__info">
+                          <div className="itemteacher__info-name">
+                            <p className="title --t3">{team?.name}</p>
+                            <span className="label badge --teacher">
+                              {team?.tags[0]}
+                            </span>
+                          </div>
+                          <h5 className="itemteacher__info-pos label">
+                            {team?.jobTitle}
+                          </h5>
+                          <p className="itemteacher__info-des">
+                            {team?.description}
+                          </p>
+                        </div>
                       </div>
-                    </div>
-                  );
-                })}
-              {/* <div className="itemteacher">
+                    );
+                  })}
+                {/* <div className="itemteacher">
                 <div className="itemteacher__avatar">
                   <img
                     src="https://cfdcircle.vn/files/avatars/VAOXpQdhq3yNvBMQlDItAYKU29ZO0gsxPTxdryL5.jpg"
@@ -1991,11 +1995,19 @@ const ContentDetailSection = ({
                   </p>
                 </div>
               </div> */}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <style jsx>{`
+        .contenteditor h2 {
+          font-family: gr;
+          font-size: 2rem;
+          line-height: 1.5;
+        }
+      `}</style>
+    </>
   );
 };
 
